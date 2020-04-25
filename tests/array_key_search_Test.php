349822cf01f07ebf1f7475b85310d8278b1e6dd5
ojullien <3778194+ojullien@users.noreply.github.com>
2020-04-25 20:11:45 +0200

    Complaint to psr-4 autoloading standard
    Signed-off-by: ojullien <3778194+ojullien@users.noreply.github.com>

--- tests/array_key_search_Test.php
+++ tests/array_key_search_Test.php
@@ -4,27 +4,27 @@ namespace PbraidersTest\Stdlib;
 
 use function Pbraiders\Stdlib\array_key_search;
 
-class ExtractDepthKeyInArrayTest extends \PHPUnit\Framework\TestCase
+class array_key_search_Test extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * Undocumented function
-     *
-     * @return array
-     */
-    public function provideSucessData(): array
-    {
-        $aData = require(__DIR__ . \DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'array_key_search.success.php');
-        return $aData;
-    }
+  /**
+   * Undocumented function
+   *
+   * @return array
+   */
+  public function provideSucessData(): array
+  {
+    $aData = require(__DIR__ . \DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'array_key_search.success.php');
+    return $aData;
+  }
 
-    /**
-     * @covers ::\Pbraiders\Stdlib\array_key_search
-     * @dataProvider provideSucessData
-     * @group specification
-     */
-    public function test_array_key_search_Success(array $input, $expected)
-    {
-        $return = array_key_search($input['array1'], $input['array2']);
-        $this->assertSame($expected, $return);
-    }
+  /**
+   * @covers ::\Pbraiders\Stdlib\array_key_search
+   * @dataProvider provideSucessData
+   * @group specification
+   */
+  public function test_array_key_search_Success(array $input, $expected)
+  {
+    $return = array_key_search($input['array1'], $input['array2']);
+    $this->assertSame($expected, $return);
+  }
 }
